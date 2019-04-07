4cb019e6d222790b98d001672fe5cc40f27eef4a
Samuele Resca <samuele.resca@gmail.com>
2019-04-07 19:48:48 +0100

    Chapter 10: Updating to .NET Core 3.0-

--- Chapter 10/Catalog/src/VinylStore.Catalog.API/Startup.cs
+++ Chapter 10/Catalog/src/VinylStore.Catalog.API/Startup.cs
@@ -3,11 +3,10 @@
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using VinylStore.Catalog.API.Infrastructure.Extensions;
-using VinylStore.Catalog.Domain.Commands.Genre;
 using VinylStore.Catalog.Domain.Infrastructure.Extensions;
 using VinylStore.Catalog.Domain.Infrastructure.Repositories;
 using VinylStore.Catalog.Infrastructure.Repositories;
@@ -35,7 +34,7 @@ namespace VinylStore.Catalog.API
                 .AddFluentValidation();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
