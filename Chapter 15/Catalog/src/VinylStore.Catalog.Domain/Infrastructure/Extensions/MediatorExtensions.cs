c2e84225d4f64ad3ee14b420131f2b5e1f2603dc
Samuele Resca <samuele.resca@gmail.com>
2019-06-18 09:46:08 +0100

    Chapter 15: Inserting fluentvalidation

--- Chapter 15/Catalog/src/VinylStore.Catalog.Domain/Infrastructure/Extensions/MediatorExtensions.cs
+++ Chapter 15/Catalog/src/VinylStore.Catalog.Domain/Infrastructure/Extensions/MediatorExtensions.cs
@@ -1,10 +1,11 @@
 using System;
 using AutoMapper;
-//using FluentValidation;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using VinylStore.Catalog.Domain.Commands.Item;
-//using VinylStore.Catalog.Domain.Commands.Item.Validators;
+using VinylStore.Catalog.Domain.Commands.Item.Validators;
+
 
 namespace VinylStore.Catalog.Domain.Infrastructure.Extensions
 {
@@ -14,10 +15,10 @@ namespace VinylStore.Catalog.Domain.Infrastructure.Extensions
         {
             services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            //
-            //            services
-            //                .AddTransient<IValidator<EditItemCommand>, EditItemCommandValidator>()
-            //                .AddTransient<IValidator<AddItemCommand>, AddItemCommandValidator>();
+
+            services
+                .AddTransient<IValidator<EditItemCommand>, EditItemCommandValidator>()
+                .AddTransient<IValidator<AddItemCommand>, AddItemCommandValidator>();
 
 
             return services;
