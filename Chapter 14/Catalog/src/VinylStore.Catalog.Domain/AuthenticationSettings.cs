c1bfc3fed6d1dc8d530c148db5ba0bcd8690b649
Samuele Resca <samuele.resca@gmail.com>
2019-08-04 18:35:09 +0100

    Revert "Adding test with invalid token (#6)"
    This reverts commit 08357c81b33ec815ec78e8eaa15b14b6001e20cb.

--- Chapter 14/Catalog/src/VinylStore.Catalog.Domain/AuthenticationSettings.cs
+++ Chapter 14/Catalog/src/VinylStore.Catalog.Domain/AuthenticationSettings.cs
@@ -3,6 +3,7 @@ namespace VinylStore.Catalog.Domain
     public class AuthenticationSettings
     {
         public string Secret { get; set; }
+        public string ConnectionString { get; set; }
         public int ExpirationDays { get; set; }
     }
 }
