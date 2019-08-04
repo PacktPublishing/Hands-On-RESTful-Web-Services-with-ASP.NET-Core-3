029bba78f428ee8768a2621cc0cf4f7751ea9487
Samuele Resca <samuele.resca@gmail.com>
2019-08-04 19:00:42 +0100

    Chapter 15 and 16 - adding invalid token test

--- Chapter 16/Catalog/src/VinylStore.Catalog.Domain/Infrastructure/Settings/AuthenticationSettings.cs
+++ Chapter 16/Catalog/src/VinylStore.Catalog.Domain/Infrastructure/Settings/AuthenticationSettings.cs
@@ -3,7 +3,6 @@ namespace VinylStore.Catalog.Domain.Infrastructure.Settings
     public class AuthenticationSettings
     {
         public string Secret { get; set; }
-        public string ConnectionString { get; set; }
         public int ExpirationDays { get; set; }
     }
 }
