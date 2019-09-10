9c5e25ce427b6642e1c347e2fd1e4f161fe6a9da
Samuele Resca <samuele.resca@gmail.com>
2019-09-11 00:31:19 +0100

    Updating Chapter 19 dependencies

--- Chapter 19/Catalog/src/VinylStore.Catalog.API/Infrastructure/Conventions/ItemApiConvention.cs
+++ Chapter 19/Catalog/src/VinylStore.Catalog.API/Infrastructure/Conventions/ItemApiConvention.cs
@@ -1,71 +1,71 @@
 namespace VinylStore.Catalog.API.Infrastructure.Conventions
 {
-  using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ApiExplorer;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
-namespace VinylStore.Catalog.API.Infrastructure.Conventions
-{
-    public static class ItemApiConvention
+    namespace VinylStore.Catalog.API.Infrastructure.Conventions
     {
+        public static class ItemApiConvention
+        {
 
-        [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(400)]
-        [ProducesDefaultResponseType]
-        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
-        public static void Get([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix),
+            [ProducesResponseType(200)]
+            [ProducesResponseType(404)]
+            [ProducesResponseType(400)]
+            [ProducesDefaultResponseType]
+            [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+            public static void Get([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix),
                                 ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
             object id)
-        {
-        }
+            {
+            }
 
-        [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(400)]
-        [ProducesDefaultResponseType]
-        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
-        public static void GetById([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix),
+            [ProducesResponseType(200)]
+            [ProducesResponseType(404)]
+            [ProducesResponseType(400)]
+            [ProducesDefaultResponseType]
+            [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+            public static void GetById([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix),
                                     ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
             object id)
-        {
-        }
+            {
+            }
 
-        [ProducesResponseType(201)]
-        [ProducesResponseType(400)]
-        [ProducesDefaultResponseType]
-        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
-        public static void Create([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any),
+            [ProducesResponseType(201)]
+            [ProducesResponseType(400)]
+            [ProducesDefaultResponseType]
+            [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+            public static void Create([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any),
                                    ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
             object model)
-        {
-        }
+            {
+            }
 
 
-        [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(400)]
-        [ProducesDefaultResponseType]
-        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
-        public static void Update([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix),
+            [ProducesResponseType(200)]
+            [ProducesResponseType(404)]
+            [ProducesResponseType(400)]
+            [ProducesDefaultResponseType]
+            [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+            public static void Update([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix),
                                    ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
             object id, [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any),
                         ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
             object model)
-        {
-        }
+            {
+            }
 
 
-        [ProducesResponseType(204)]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(400)]
-        [ProducesDefaultResponseType]
-        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
-        public static void Delete([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix),
+            [ProducesResponseType(204)]
+            [ProducesResponseType(404)]
+            [ProducesResponseType(400)]
+            [ProducesDefaultResponseType]
+            [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+            public static void Delete([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix),
                                    ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
             object id)
-        {
+            {
+            }
         }
     }
-}
 
 }
