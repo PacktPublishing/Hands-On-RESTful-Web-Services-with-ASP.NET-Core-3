4432ab882078702c8484fab66b5b783ef13c2538
Samuele Resca <samuele.resca@gmail.com>
2019-10-20 17:48:00 +0100

    Updating Chapter 12

---
+++ Chapter 12/Catalog/src/Catalog.Domain/Requests/Item/EditItemRequest.cs
@@ -0,0 +1,20 @@
+using System;
+using Catalog.Domain.Entities;
+
+namespace Catalog.Domain.Requests.Item
+{
+    public class EditItemRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string LabelName { get; set; }
+        public Price Price { get; set; }
+        public string PictureUri { get; set; }
+        public DateTimeOffset ReleaseDate { get; set; }
+        public string Format { get; set; }
+        public int AvailableStock { get; set; }
+        public Guid GenreId { get; set; }
+        public Guid ArtistId { get; set; }
+    }
+}
