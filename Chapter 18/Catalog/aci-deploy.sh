b5b7cbb43606b3557c34fd22ff71b7bbd7c35936
Samuele Resca <samuele.resca@gmail.com>
2019-11-24 16:56:21 +0000

    Updating aci-deploy.sh script

--- Chapter 18/Catalog/aci-deploy.sh
+++ Chapter 18/Catalog/aci-deploy.sh
@@ -4,26 +4,24 @@
 export resource_group=catalogServiceGroup
 # Set the sql container name
 export sql_name=catalog-db
-# Set the sql admin password
-export sql_admin_pass=P@ssw0rd
+# Set the cache container name
+export cache_name=catalog-cache
 # Set the event service bus name
 export esb_name=catalog-esb
+# Set the service name
+export api_name=catalogapi
+# Set the sql admin password
+export sql_admin_pass=P@ssw0rd
 # Set the event service bus username
 export rabbitmq_user=guest
 # Set the event service bus password
 export rabbitmq_pass=guest
-# Set the cache container name
-export cache_name=catalog-cache
-# Set the service group name
-export resource_group=catalogServiceGroup
-# Set the service name
-export api_name=catalogapi
 # Set the api ASPNETCORE_ENVIRONMENT variables
 export environment=Stage
+# Set the registry username
+export registry_username=<registry_username>
 # Set the registry password
-export registry_password=<registry_password>
-
-
+export registry_password=<>
 
 az container create --resource-group ${resource_group} \
                     --location westeurope \
@@ -65,5 +63,6 @@ az container create --resource-group ${resource_group} \
                     --dns-name-label ${api_name} \
                     --ip-address public \
                     --ports 80 \
-                    --environment-variables ASPNETCORE_ENVIRONMENT=${environment} --registry-password=${registry_password}
-
+                    --environment-variables ASPNETCORE_ENVIRONMENT=${environment} \
+                    --registry-password=${registry_password} \
+                    --registry-username=${registry_username}
