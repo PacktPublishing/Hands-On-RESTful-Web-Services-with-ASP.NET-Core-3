54e1b4386e347171c5a601bf9bfbbcad12bbb19a
Samuele Resca <samuele.resca@gmail.com>
2019-12-04 14:01:25 +0000

    Executing update-packages.sh (#40)

--- update-packages.sh
+++ update-packages.sh
@@ -1,17 +1,17 @@
-dotnet outdated "Chapter 8/Catalog" -u --pre-release Always
-dotnet outdated "Chapter 9/Catalog" -u --pre-release Always
-dotnet outdated "Chapter 10/Catalog" -u --pre-release Always
-dotnet outdated "Chapter 11/Catalog" -u --pre-release Always
-dotnet outdated "Chapter 12/Catalog" -u --pre-release Always
-dotnet outdated "Chapter 13/Catalog" -u --pre-release Always
-dotnet outdated "Chapter 13/Cart" -u --pre-release Always
-dotnet outdated "Chapter 15/Catalog" -u --pre-release Always
-dotnet outdated "Chapter 15/Cart" -u --pre-release Always
-dotnet outdated "Chapter 16/Catalog" -u --pre-release Always
-dotnet outdated "Chapter 16/Cart" -u --pre-release Always
-dotnet outdated "Chapter 17/Catalog" -u --pre-release Always
-dotnet outdated "Chapter 17/Cart" -u --pre-release Always
-dotnet outdated "Chapter 18/Catalog" -u --pre-release Always
-dotnet outdated "Chapter 18/Cart" -u --pre-release Always
-dotnet outdated "Chapter 19/Catalog" -u --pre-release Always
-dotnet outdated "Chapter 19/Cart" -u --pre-release Always
+dotnet outdated "Chapter 8/Catalog" -u 
+dotnet outdated "Chapter 9/Catalog" -u 
+dotnet outdated "Chapter 10/Catalog" -u 
+dotnet outdated "Chapter 11/Catalog" -u 
+dotnet outdated "Chapter 12/Catalog" -u 
+dotnet outdated "Chapter 13/Catalog" -u 
+dotnet outdated "Chapter 13/Cart" -u 
+dotnet outdated "Chapter 15/Catalog" -u 
+dotnet outdated "Chapter 15/Cart" -u 
+dotnet outdated "Chapter 16/Catalog" -u 
+dotnet outdated "Chapter 16/Cart" -u 
+dotnet outdated "Chapter 17/Catalog" -u 
+dotnet outdated "Chapter 17/Cart" -u 
+dotnet outdated "Chapter 18/Catalog" -u 
+dotnet outdated "Chapter 18/Cart" -u 
+dotnet outdated "Chapter 19/Catalog" -u 
+dotnet outdated "Chapter 19/Cart" -u 
