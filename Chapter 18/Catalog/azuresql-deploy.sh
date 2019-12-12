b595ae96c7721aba9c76257227d7b367b73f93cd
Samuele Resca <samuele.resca@gmail.com>
2019-12-12 14:20:17 +0000

    Updating Chapter 18 and Chapter 19 scrips

--- Chapter 18/Catalog/azuresql-deploy.sh
+++ Chapter 18/Catalog/azuresql-deploy.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 
-# Set an admin login and password for your database
-export user_admin=samueler
-export user_pass=P@ssW0rd
+# Set an user login and password for your database
+export user_admin=catalog_srv
+export user_pass=P@ssw0rd
 # The logical server name has to be unique in the system
-export server_name=catalogsql
-export database_name=catalog
+export server_name=storecatalogapi
+# Set the database name
+export database_name=Store
 # The resource group name
-export resourceGroup=catalogapiDataResourceGroup
+export resourceGroup=handsOnAppService
 # The ip address range that you want to allow to access your DB
 export startip=0.0.0.0
 export endip=0.0.0.0
 
 # Create a resource group
 az group create \
-	--name ${resourceGroup} \
-	--location westeurope
+   --name ${resourceGroup} \
+   --location westeurope
 
 # Create a logical server in the resource group
 az sql server create \
-	--name ${server_name} \
-	--resource-group ${resourceGroup} \
-	--location westeurope  \
-	--admin-user ${user_admin} \
-	--admin-password ${user_pass}
+   --name ${server_name} \
+   --resource-group ${resourceGroup} \
+   --location westeurope  \
+   --admin-user ${user_admin} \
+   --admin-password ${user_pass}
 
 # Configure a firewall rule and open to local Azure services
 az sql server firewall-rule create \
-	--resource-group ${resourceGroup} \
-	--server ${server_name} \
-	-n AllowYourIp \
-	--start-ip-address ${startip} \
-	--end-ip-address ${endip}
+   --resource-group ${resourceGroup} \
+   --server ${server_name} \
+   -n AllowYourIp \
+   --start-ip-address ${startip} \
+   --end-ip-address ${endip}
 
 # Create a database in the server 
 az sql db create \
-	--resource-group ${resourceGroup} \
-	--server ${server_name} \
-	--name ${database_name} \
-	--service-objective S0 \
+   --resource-group ${resourceGroup} \
+   --server ${server_name} \
+   --name ${database_name} \
+   --service-objective S0 \
     --zone-redundant false
--- Chapter 19/Catalog/appservice-deploy.sh
+++ Chapter 19/Catalog/appservice-deploy.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
 # Set the service group name
-export resource_group=catalogServiceGroup
+export resource_group=handsOnAppService
 # Set the plan
 export plan=catalogServicePlan
 # Set the service name
 export app_service_name=catalog-srv
 # Set the api ASPNETCORE_ENVIRONMENT variables
-export environment=StageAppService
+export environment=StageAppServices
+# Defines the ACR registry URL
+export registry_address=<registry_name>.azurecr.io
 
 # Create a resource group
 az group create \
-	--name ${resource_group} \
-	--location westeurope
+   --name ${resource_group} \
+   --location westeurope
 
-az appservice plan create \
-    --name ${plan} \
-    --resource-group ${resource_group} \
-    --sku B1 \
-    --is-linux
-
+# Create the app service
 az webapp create --resource-group ${resource_group} \
-                 --plan ${plan} \
-                 --name ${app_service_name} \
-                 --deployment-container-image-name myprivatecontainers.azurecr.io/catalog_api:v1
-
+                             --plan ${plan} \
+                             --name ${app_service_name} \
+                             --deployment-container-image-name ${registry_address}/catalog_api:v1
+                             
+# Set the ASPNETCORE_ENVIRONMENT variable
 az webapp config appsettings set -g ${resource_group} \
-                                 -n ${app_service_name} \
-                                 --settings ASPNETCORE_ENVIRONMENT=${environment}
+                                             -n ${app_service_name} \
+                                             --settings ASPNETCORE_ENVIRONMENT=${environment}
