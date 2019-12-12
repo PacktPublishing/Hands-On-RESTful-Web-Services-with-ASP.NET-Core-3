bdefc4e0b0873f6d8eb13fb28232d2cb4818ce1b
Samuele Resca <samuele.resca@gmail.com>
2019-12-12 14:40:01 +0000

    Updating Chapter 18 and Chapter 19 scrips (#45)

--- Chapter 19/Catalog/aci-deploy.sh
+++ Chapter 19/Catalog/aci-deploy.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
-
 # Set the service group name
-export resource_group=catalogServiceGroup
+export resource_group=handsOnServices
+# Set the registry address
+export registry_address = <registry_address>.azurecr.io
+# Set the registry username
+export registry_username = <registry_username>
+# Set the registry password
+export registry_password = <registry_password>
 # Set the sql container name
 export sql_name=catalog-db
-# Set the cache container name
-export cache_name=catalog-cache
-# Set the event service bus name
-export esb_name=catalog-esb
-# Set the service name
-export api_name=catalogapi
 # Set the sql admin password
 export sql_admin_pass=P@ssw0rd
+# Set the event service bus name
+export esb_name=catalog-esb
 # Set the event service bus username
 export rabbitmq_user=guest
 # Set the event service bus password
 export rabbitmq_pass=guest
+# Set the cache container name
+export cache_name=catalog-cache
+# Set the service name
+export api_name=catalog-api
 # Set the api ASPNETCORE_ENVIRONMENT variables
 export environment=Stage
-# Set the registry username
-export registry_username=<registry_username>
-# Set the registry password
-export registry_password=<>
 
 az container create --resource-group ${resource_group} \
                     --location westeurope \
@@ -30,7 +31,6 @@ az container create --resource-group ${resource_group} \
                     --cpu 1 \
                     --memory 1 \
                     --dns-name-label ${sql_name} \
-                    --ip-address public \
                     --ports 1433 \
                     --environment-variables ACCEPT_EULA=Y SA_PASSWORD=${sql_admin_pass}
                     
@@ -41,7 +41,6 @@ az container create --resource-group ${resource_group} \
                     --cpu 1 \
                     --memory 1 \
                     --dns-name-label ${esb_name} \
-                    --ip-address public \
                     --ports 5672 \
                     --environment-variables RABBITMQ_DEFAULT_USER=${rabbitmq_user} RABBITMQ_DEFAULT_PASS=${rabbitmq_pass} 
 
@@ -51,19 +50,17 @@ az container create --resource-group ${resource_group} \
                     --cpu 1 \
                     --memory 1 \
                     --dns-name-label ${cache_name} \
-                    --ip-address public \
                     --ports 6379 
 
 az container create --resource-group ${resource_group} \
                     --location westeurope \
                     --name ${api_name} \
-                    --image myprivatecontainers.azurecr.io/catalog_api:v1 \
+                    --image ${registry_address}/catalog_api:v1 \
                     --cpu 1 \
                     --memory 1 \
                     --dns-name-label ${api_name} \
-                    --ip-address public \
                     --ports 80 \
+                    --ip-address public \
                     --environment-variables ASPNETCORE_ENVIRONMENT=${environment} \
                     --registry-password=${registry_password} \
                     --registry-username=${registry_username}
-
